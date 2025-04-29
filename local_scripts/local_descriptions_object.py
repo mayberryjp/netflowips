ec00d9fc6b7254ba081404524c5d13da1484e430
JR Mayberry <rimayber@microsoft.com>
2025-04-29 16:16:08 +0900

    prepared some code to update category on all devices

---
+++ local_scripts/local_descriptions_object.py
@@ -0,0 +1,14 @@
+LOCAL_DESCRIPTIONS = [
+        {
+            "api_endpoint": "http://192.168.230.236:8044",
+            "ip_address": "192.168.230.1",
+            "category": "FIREWALL",
+            "local_description": "PfSense Firewall"
+        },
+        {
+            "api_endpoint": "http://192.168.230.236:8044",
+            "ip_address": "192.168.230.236",
+            "category": "SERVER",
+            "local_description": "Farm Ubuntu Server/Docker"
+        }
+]
